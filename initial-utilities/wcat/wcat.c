ddb1047e808716609341a1531405c1f181b0ddc6
jay3ss <mentoce@gmail.com>
2024-08-22 12:09:18 -0700

    Finish wcat, pass all tests

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,30 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_file(FILE* file) {
+    size_t length = 4096;
+    char* line = malloc(sizeof(char) * length);
+
+    while (fgets(line, length, file) != NULL) {
+        printf("%s", line);
+    }
+    free(line);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            FILE* file = fopen(argv[i], "r");
+            if (file == NULL) { 
+                puts("wcat: cannot open file");
+                return 1; 
+            }
+            print_file(file);
+            fclose(file);
+        }
+    } 
+
+    return 0;
+}
